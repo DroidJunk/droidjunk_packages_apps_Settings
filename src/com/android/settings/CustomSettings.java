47059dc6d5f38e8454c9ba5bf42c7646b8e1a798
Jimmy Miller <jbmiller148@gmail.com>
2011-12-26 09:16:40 -0600

    Initial Commit

---
+++ src/com/android/settings/CustomSettings.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings;
+
+
+
+
+
+import static android.provider.Settings.System.SCREEN_OFF_TIMEOUT;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
+import android.view.IWindowManager;
+import android.view.Surface;
+
+import java.util.ArrayList;
+
+public class CustomSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    
+	private static final String TAG = "CustomSettings";
+    
+	private static final String SHOW_CARRIER = "show_carrier";
+	private static final String CARRIER_COLOR = "carrier_color";
+	private static final String CARRIER_CUSTOM = "carrier_custom";
+	private static final String CARRIER_CUSTOM_TEXT = "carrier_custom_text";
+    
+    private CheckBoxPreference mShowCarrier;
+    private CheckBoxPreference mCarrierCustom;
+
+    private Preference mCarrierColor;
+    private Preference mCarrierCustomText;
+
+	
+	
+	
+	
+    /** If there is no setting in the provider, use this. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        addPreferencesFromResource(R.xml.custom_settings);
+        
+        mShowCarrier = (CheckBoxPreference) findPreference(SHOW_CARRIER);
+		mShowCarrier.setOnPreferenceChangeListener(this);
+        mCarrierCustom = (CheckBoxPreference) findPreference(CARRIER_CUSTOM);
+		mCarrierCustom.setOnPreferenceChangeListener(this);
+        
+
+   }
+    
+    
+
+
+
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        
+   
+                
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+       
+    }
+
+ 
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+  
+    	final String key = preference.getKey();
+        if (SHOW_CARRIER.equals(key)) {
+            //int value = Integer.parseInt((String) objValue);
+
+            int value = (Boolean) objValue? 1 : 0;
+
+
+            try {
+                Settings.System.putInt(getContentResolver(), Settings.System.STATUSBAR_CARRIER_SHOW, value);
+            } catch (NumberFormatException e) {
+                Log.e(TAG, "could not save value", e);
+            }
+        }
+    	
+    	
+  	
+
+
+        return true;
+    }
+}
